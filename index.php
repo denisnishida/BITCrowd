2e552fb95e43503d8cc6d76b97c213676222b4b6
Denis T. Nishida <denis.nishida@gmail.com>
2017-01-13 11:01:51 -0600

    Added Denis profile

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
         <title>GIT Lesson</title>
     </head>
     <body>
-        
+        <p>Denis Nishida: Student at Red River College at Business Information Technology. Looks asian but is brazilian. Likes pasta and double-doubles.</p>
     </body>
  </html>
