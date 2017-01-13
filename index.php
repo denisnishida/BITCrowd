ec5d2568e66a320a25dfd67c4b07958a9263be42
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2017-01-13 11:17:01 -0600

    Code cleanup
    Cleaned up the merge

--- index.php
+++ index.php
@@ -8,16 +8,14 @@
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
-<<<<<<< HEAD
-        <div class='rj'>
+        <div id='rj'>
           <h2>Yo</h2>
           <p>Just a kid at heart with adult responsiblities.</p>
-=======
+        </div>
         <div id="harryKainth">
             <p>
                 My name is Harry Kainth, I enjoy long walks on the beach.
             </p>
->>>>>>> refs/remotes/origin/master
         </div>
     </body>
  </html>
