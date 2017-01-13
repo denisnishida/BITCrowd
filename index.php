7977f6b6e74631fc315bd54e00d5891927c5793b
JadeBuhler <buhler.jb.jb@gmail.com>
2017-01-13 11:13:49 -0600

    Added my "About Me" paragraph.

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
-
+     <div>
+        <p>Jade Buhler: Born and raised in the harsh Winnipeg cold this debonair individual is an aspiring web developer.
+        When not busy with work or school you can find him relaxing at home watching Netfilx or jamming on the guitar. </p>
+     </div>
     </body>
  </html>
