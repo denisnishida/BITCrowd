9a6d5e1e8bcfde8d0cedff250d6a88e908342903
kainth01 <kainth247@gmail.com>
2017-01-13 11:06:38 -0600

    added first sentance of my paragraph

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
         <title>GIT Lesson</title>
     </head>
     <body>
-        
+        <div id="harryKainth">
+            <p>
+                My name is Harry Kainth, I enjoy long walks on the beach.
+            </p>
+        </div>
     </body>
  </html>
