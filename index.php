4506f0631e2eb8d037868223c37a2f1b0b932fc5
Denis T. Nishida <denis.nishida@gmail.com>
2017-01-13 11:06:49 -0600

    Deleting merging trash

--- index.php
+++ index.php
@@ -8,10 +8,6 @@
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
     <body>
-<<<<<<< HEAD
         <p>Denis Nishida: Student at Red River College at Business Information Technology. Looks asian but is brazilian. Likes pasta and double-doubles.</p>
-=======
-
->>>>>>> refs/remotes/origin/master
     </body>
  </html>
