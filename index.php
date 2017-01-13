20bb8387429c4a816ab4fe70d63aa2d4353f0486
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2017-01-13 11:07:12 -0600

    index.php rj update

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         <title>GIT Lesson</title>
     </head>
     <body>
-        
+        <div class='rj'>
+          <h2>Yo</h2>
+          <p>Just a kid at heart with adult responsiblities.</p>
+        </div>
     </body>
  </html>
